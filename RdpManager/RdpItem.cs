3cf68261333eb63952ab58b61f2a16b3c075f625
AmirNorouzpour <Amirnorozpour@gmail.com>
2021-01-06 16:43:21 +0330

    Fix Port And Exit Path

--- RdpManager/RdpItem.cs
+++ RdpManager/RdpItem.cs
@@ -13,16 +13,22 @@ namespace RdpManager
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var path0 = Main.DataPath + "//" + textBox_server.Text;
+            if (!Directory.Exists(path0))
+                Directory.CreateDirectory(path0);
+
             var str = RdpHandler.Process(new LogInfo
             {
                 Domain = textBox_Domain.Text,
-                IpAddress = textBox_server.Text + ":" + textBox_port.Text,
+                IpAddress = !string.IsNullOrWhiteSpace(textBox_port.Text)
+                    ? textBox_server.Text + ":" + textBox_port.Text
+                    : textBox_server.Text,
                 Password = textBox_password.Text,
                 Username = textBox_username.Text,
                 MultiMonitor = checkBox_multimon.Checked ? 1 : 0
             });
 
-            var path = Main.DataPath + "//" + textBox_server.Text + "//" + textBox_name.Text + ".rdp";
+            var path = path0 + "//" + textBox_name.Text + ".rdp";
             File.WriteAllText(path, str);
             DialogResult = DialogResult.OK;
         }
